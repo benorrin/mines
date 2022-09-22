f9ff430526aecc0529b3e4dcf56b1e75b0609213
Ben Orrin <skylonrow54@googlemail.com>
2022-09-22 23:29:32 +0100

    Fixed login indentation
    fixed indentation and formatting for login component

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -9,23 +9,17 @@ import {
     Link,
     Button,
     Heading,
-    Text,
-    useColorModeValue,
+    Text
     } from '@chakra-ui/react';
-  
-export default function SimpleCard() {
+
+export default function Login(props) {
     return (
-        <Flex
-            align={'center'}
-            justify={'center'}>
+        <Flex align={'center'} justify={'center'}>
             <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
                 <Stack align={'center'}>
                     <Heading fontSize={'4xl'}>Sign in to your account</Heading>
                 </Stack>
-                <Box
-                    rounded={'lg'}
-                    boxShadow={'lg'}
-                    p={4}>
+                <Box rounded={'lg'} boxShadow={'lg'} p={4}>
                     <Stack spacing={4}>
                         <FormControl id="email">
                             <FormLabel>Email address</FormLabel>
@@ -36,18 +30,11 @@ export default function SimpleCard() {
                             <Input type="password" />
                         </FormControl>
                         <Stack spacing={10}>
-                            <Stack
-                            direction={{ base: 'column', sm: 'row' }}
-                            align={'start'}
-                            justify={'space-between'}>
-                            <Checkbox>Remember me</Checkbox>
-                            <Link color={'blue.400'}>Forgot password?</Link>
+                            <Stack direction={{ base: 'column', sm: 'row' }} align={'start'} justify={'space-between'}>
+                                <Checkbox>Remember me</Checkbox>
+                                <Link color={'blue.400'}>Forgot password?</Link>
                             </Stack>
-                            <Button
-                                bg={'blue.400'}
-                                color={'white'}
-                                _hover={{
-                                    bg: 'blue.500'}}>
+                            <Button bg={'blue.400'} color={'white'} _hover={{bg: 'blue.500'}}>
                                 Sign in
                             </Button>
                         </Stack>
@@ -61,4 +48,4 @@ export default function SimpleCard() {
             </Stack>
         </Flex>
     );
-  }
+}
