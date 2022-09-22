38bcd5e3d3d929f92a98ce29affd0fe5fd8665b2
Ben Orrin <skylonrow54@googlemail.com>
2022-09-22 22:38:48 +0100

    Added navbar component
    Added dom routing, and template components for react frontend

---
+++ client/src/components/home.js
@@ -0,0 +1,8 @@
+import { Outlet, Link } from "react-router-dom";
+
+const Home = () => {
+    return (
+        <h1>Home</h1>
+)};
+
+export default Home;
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react';
+import Navbar from "./components/navbar.js";
+import Home from "./components/home.js";
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Navbar/>
+            <Routes>
+                <Route path="/" element={<Home />}>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ChakraProvider>
+        <App />
+    </ChakraProvider>
 );
