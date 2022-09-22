71874fcbf4d2b7feac07321ab1db2a40a10e8b81
Ben Orrin <skylonrow54@googlemail.com>
2022-09-22 23:54:13 +0100

    Refactored auth form
    Refactored login form template to a standardized component for reuse in signup form - authform.
    
    Created parent login component class to handle login logic.
    
    Removed unneeded imports on authform

---
+++ client/src/components/authform.js
@@ -0,0 +1,49 @@
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    Stack,
+    Link,
+    Button,
+    Heading,
+    Text
+    } from '@chakra-ui/react';
+
+export default function AuthForm(props) {
+    return (
+        <Flex align={'center'} justify={'center'}>
+            <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+                <Stack align={'center'}>
+                    <Heading fontSize={'4xl'}>Sign in to your account</Heading>
+                </Stack>
+                <Box rounded={'lg'} boxShadow={'lg'} p={4}>
+                    <Stack spacing={4}>
+                        <FormControl id="email">
+                            <FormLabel>Email address</FormLabel>
+                            <Input type="email" />
+                        </FormControl>
+                        <FormControl id="password">
+                            <FormLabel>Password</FormLabel>
+                            <Input type="password" />
+                        </FormControl>
+                        <Stack spacing={10}>
+                            <Stack direction={{ base: 'column', sm: 'row' }} align={'start'} justify={'space-between'}>
+                                <Link color={'blue.400'}>Forgot password?</Link>
+                            </Stack>
+                            <Button bg={'blue.400'} color={'white'} _hover={{bg: 'blue.500'}}>
+                                Sign in
+                            </Button>
+                        </Stack>
+                        <Stack pt={6}>
+                            <Text align={'center'}>
+                                Don't have an account yet? <Link color={'blue.400'}>Register</Link>
+                            </Text>
+                        </Stack>
+                    </Stack>
+                </Box>
+            </Stack>
+        </Flex>
+    );
+}
