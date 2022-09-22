0b5f6dee39359ebae0a2da67f22fe3a67e6c726d
Ben Orrin <skylonrow54@googlemail.com>
2022-09-22 23:16:04 +0100

    Added login component
    Added login form component to client.
    
    Removed navbar bottom margin

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ChakraProvider } from '@chakra-ui/react';
 import Navbar from "./components/navbar.js";
 import Home from "./components/home.js";
+import Login from "./components/login.js";
 
 export default function App() {
     return (
         <BrowserRouter>
             <Navbar/>
             <Routes>
-                <Route path="/" element={<Home />}>
-                </Route>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/login" element={<Login />}></Route>
             </Routes>
         </BrowserRouter>
     );
