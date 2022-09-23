6eeeb354555228de25001c354f5df9de7f0c8826
Ben Orrin <skylonrow54@googlemail.com>
2022-09-23 13:32:55 +0100

    Added auth controller
    Added auth controller to handle passport login/signup

---
+++ controllers/auth.js
@@ -0,0 +1,60 @@
+require('dotenv').config();
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const JWTstrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+const UserModel = require('../models/user.js');
+
+passport.use('signup', new localStrategy({
+        usernameField: 'username',
+        passwordField: 'password'
+    },
+    async (username, password, done) => {
+        try {
+            const user = await UserModel.create({ username, password });
+
+            return done(null, user);
+        } catch (error) {
+            done(error);
+        }
+    }
+));
+
+passport.use('login', new localStrategy({
+        usernameField: 'username',
+        passwordField: 'password'
+    },
+    async (username, password, done) => {
+        try {
+            const user = await UserModel.findOne({ username });
+
+            if (!user) {
+                return done(null, false, { message: 'User not found' });
+            }
+
+            const validate = await user.isValidPassword(password);
+
+            if (!validate) {
+                return done(null, false, { message: 'Wrong Password' });
+            }
+
+            return done(null, user, { message: 'Logged in Successfully' });
+        } catch (error) {
+            return done(error);
+        }
+    }
+));
+
+passport.use(new JWTstrategy({
+        secretOrKey: process.env.SECRET_KEY,
+        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken()
+    },
+    async (token, done) => {
+        try {
+            return done(null, token.user);
+        } catch (error) {
+            done(error);
+        }
+    }
+)
+);
