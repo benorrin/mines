827060f458422794ee0648c1982681c2c536233c
Ben Orrin <skylonrow54@googlemail.com>
2022-09-27 00:54:55 +0100

    Fixed wrong file
    Added function to wrong file

--- client/src/components/register.js
+++ client/src/components/register.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import AuthForm from "./authform.js";
 
 class Register extends React.Component{
@@ -9,6 +10,21 @@ class Register extends React.Component{
 
     submit(values){
         console.log(values);
+        axios.post('http://localhost:3000/register', {
+            username: values.username,
+            password: values.password
+        }).then(response => {
+            console.log(response)
+            if(response.data) {
+                console.log("SIGNUP: Signup successful")
+                this.props.navigate('/login');
+            } else {
+                console.log("SIGNUP: Signup error")
+            }
+        }).catch(error => {
+            console.log("SIGNUP: Signup error")
+            console.log(error)
+        });
     }
 
     render(){
