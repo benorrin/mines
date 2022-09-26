a736ed1c96369e10e4a66e21b95f0267f4aa0a21
Ben Orrin <skylonrow54@googlemail.com>
2022-09-27 00:58:06 +0100

    Added CORS
    Added CORS support to backend

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require("cors");
 
 /*
 *  Controller Imports
@@ -21,6 +22,7 @@ var registerRouter = require('./routes/register.js');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
