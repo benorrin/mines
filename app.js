9a41526eeeec844c2465554d04e126fbef75334d
Ben Orrin <skylonrow54@googlemail.com>
2022-10-04 13:37:55 +0100

    Added session to app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require("cors");
+const session = require('express-session');
 const passport = require('passport');
 
 /*
@@ -24,6 +25,25 @@ var registerRouter = require('./routes/register.js');
 
 var app = express();
 
+
+/*
+*  Passport
+*/
+
+app.use(session({
+  secret: 'em*LE#z#qYz#G%HactjPBNW@*9T88FgV3NW%X^YMmuwWhpcMuhUBWYKs^awVTsKa',
+  resave: false,
+  saveUninitialized: false
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+/*
+*  App Declarations
+*/
+
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
@@ -31,7 +51,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// view engine setup
+
+/*
+*  View Engine
+*/
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -44,18 +68,24 @@ app.use('/', indexRouter);
 app.use('/register', registerRouter);
 
 
-// catch 404 and forward to error handler
+/*
+*  404 Handler
+*/
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
+/*
+*  Error Handler
+*/
+
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.json({
     message: err.message
