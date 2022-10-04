d3432c60df5807a6b0c1f9dc58288bd2342dfe4b
Ben Orrin <skylonrow54@googlemail.com>
2022-10-04 21:29:08 +0100

    Added getUser function to login controller

--- controllers/login.js
+++ controllers/login.js
@@ -20,3 +20,27 @@ async function login(){
         return next(error);
     }
 }
+
+
+/**
+* Returns a user object for a given username
+* 
+* @param {string} username Username of user to retrieve
+* 
+* @returns {object} Returns user object if success. Will throw an error if user does not exist, or if retrieval failed.
+*/
+
+async function getUser(username){
+    try{
+        const user = await UserModel.findOne({ username });
+
+        if (!user) {
+            const error = new Error('User not found');
+            throw error;
+        }
+
+        return user;
+    } catch(error){
+        throw error;
+    }
+}
