07eac65e73b74dc51ca1d6bc3dca1b69ff30079d
Ben Orrin <skylonrow54@googlemail.com>
2022-10-04 11:42:28 +0100

    Added error handling to register router

--- routes/register.js
+++ routes/register.js
@@ -2,13 +2,16 @@ var express = require('express');
 var router = express.Router();
 const passport = require('passport');
 
-router.post('/', passport.authenticate('register', { session: false }),
-    async (req, res, next) => {
+router.post('/', function(req, res, next) {
+    passport.authenticate('register', { session: false }, async function(err, user, info) {
+        if (err) {
+            return next(err);
+        }
         res.json({
             message: 'Register successful',
             user: req.user
         });
-    }
-);
+    })(req, res, next);
+  });
 
 module.exports = router;
