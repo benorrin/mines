4909b595b88bdc0305654e286c237b731e822c4f
Ben Orrin <skylonrow54@googlemail.com>
2022-10-15 18:42:26 +0100

    Push tfstate to s3 to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,12 @@
 terraform {
   required_version = ">=0.12.13"
+  backend "s3" {
+    bucket         = "solmines-terraform-bucket"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "aws-locks"
+    encrypt        = true
+  }
 }
 
 # Download any stable version in AWS of 2.36.0 or higher
@@ -10,6 +17,6 @@ provider "aws" {
 # Call the tfbootstrap module to built tfstate S3 bucket + DB
 module "tfbootstrap" {
   source                      = "./modules/tfbootstrap"
-  name_of_s3_bucket           = "solmines_terraform_bucket"
+  name_of_s3_bucket           = "solmines-terraform-bucket"
   dynamo_db_table_name        = "aws-locks"
 }
