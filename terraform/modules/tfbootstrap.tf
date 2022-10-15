c081fb1c3b34fb383d510e5098069af9dd894914
Ben Orrin <skylonrow54@googlemail.com>
2022-10-15 18:21:39 +0100

    Create terraform bootstrap module

---
+++ terraform/modules/tfbootstrap.tf
@@ -0,0 +1,46 @@
+####
+# Module to build S3 bucket and DB to hold terraform state
+####
+
+
+# Create S3 bucket to store TF state
+resource "aws_s3_bucket" "state_bucket" {
+  bucket = var.name_of_s3_bucket
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Terraform = "true"
+  }
+}
+
+# Build a DynamoDB to use for terraform state locking
+resource "aws_dynamodb_table" "tf_lock_state" {
+  name = var.dynamo_db_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name    = var.dynamo_db_table_name
+    BuiltBy = "Terraform"
+  }
+}
