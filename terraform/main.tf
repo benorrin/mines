031435477371a886c81bf85a9c1d97cb11f78c14
Ben Orrin <skylonrow54@googlemail.com>
2022-10-15 18:17:39 +0100

    Create terraform main.tf
    Created basic terraform main.tf to bootstrap S3 bucket + DB to store TF state

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">=0.12.13"
+}
+
+# Download any stable version in AWS of 2.36.0 or higher
+provider "aws" {
+  region  = "us-east-1"
+  version = "~> 2.36.0"
+}
+
+# Call the tfbootstrap module to built tfstate S3 bucket + DB
+module "tfbootstrap" {
+  source                      = "./modules/tfbootstrap"
+  name_of_s3_bucket           = "solmines_terraform_bucket"
+  dynamo_db_table_name        = "aws-locks"
+}
