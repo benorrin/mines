8ddb1828728588158666158c53b69f6e8ca26f8e
Ben Orrin <skylonrow54@googlemail.com>
2022-10-17 00:26:33 +0100

    Create terraform IGW

---
+++ terraform/gateway.tf
@@ -0,0 +1,11 @@
+####
+# Internet Gateway Declaration
+####
+
+resource "aws_internet_gateway" "solmines-igw" {
+    vpc_id = "${aws_vpc.solmines-vpc.id}"
+
+    tags {
+        Name = "solmines-igw"
+    }
+}
