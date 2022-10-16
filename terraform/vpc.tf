7124e3a72379c700a910a881f732bdb9f5f2e412
Ben Orrin <skylonrow54@googlemail.com>
2022-10-17 00:14:32 +0100

    Add terraform vpc

---
+++ terraform/vpc.tf
@@ -0,0 +1,15 @@
+####
+# VPC Declaration
+####
+
+resource "aws_vpc" "solmines-vpc" {
+    cidr_block = "10.0.0.0/16"
+    enable_dns_support = "true"
+    enable_dns_hostnames = "true"
+    enable_classiclink = "false"
+    instance_tenancy = "default"    
+    
+    tags {
+        Name = "solmines-vpc"
+    }
+}
