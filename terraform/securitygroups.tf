e0781753069a694379d0a79c6c2dba5e53c12511
Ben Orrin <skylonrow54@googlemail.com>
2022-10-17 00:48:39 +0100

    Create terraform security groups

---
+++ terraform/securitygroups.tf
@@ -0,0 +1,22 @@
+####
+# Security Group Declarations
+####
+
+# Create database security group 
+resource "aws_security_group" "solmines-database" {
+vpc_id      = "${aws_vpc.solmines-vpc.id}"
+
+  ingress {
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.1.0/24"]
+  }
+}
