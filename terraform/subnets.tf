7762d79807a27f12cc7835517cd42a53d41356fd
Ben Orrin <skylonrow54@googlemail.com>
2022-10-17 00:53:41 +0100

    Add private subnet 2 terraform

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -14,14 +14,26 @@ resource "aws_subnet" "solmines-public-subnet" {
     }
 }
 
-# Create private subnet
-resource "aws_subnet" "solmines-private-subnet" {
+# Create private subnet 1
+resource "aws_subnet" "solmines-private-subnet1" {
     vpc_id = "${aws_vpc.solmines-vpc.id}"
-    cidr_block = "10.0.2.0/24"
+    cidr_block = "10.0.3.0/24"
     map_public_ip_on_launch = "true"
     availability_zone = "us-east-1a"
 
     tags = {
-        Name = "solmines-private-subnet"
+        Name = "solmines-private-subnet1"
+    }
+}
+
+# Create private subnet 2
+resource "aws_subnet" "solmines-private-subnet2" {
+    vpc_id = "${aws_vpc.solmines-vpc.id}"
+    cidr_block = "10.0.4.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "us-east-1b"
+
+    tags = {
+        Name = "solmines-private-subnet2"
     }
 }
