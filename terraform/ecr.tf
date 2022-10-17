dca136e30bb5c40709f8d3dee476c48d7acf92e7
Ben Orrin <skylonrow54@googlemail.com>
2022-10-17 20:50:51 +0100

    Added terraform ECR

---
+++ terraform/ecr.tf
@@ -0,0 +1,34 @@
+####
+# ECR Declarations
+####
+
+resource "aws_ecr_repository" "solmines-ecr-repository" {
+  name                 = "solmines-ecr-repository"
+  image_tag_mutability = "IMMUTABLE"
+}
+
+resource "aws_ecr_repository_policy" "solmines-ecr-repository-policy" {
+  repository = aws_ecr_repository.solmines-ecr-repository.name
+  policy     = <<EOF
+  {
+    "Version": "2008-10-17",
+    "Statement": [
+      {
+        "Sid": "adds full ecr access to the demo repository",
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:BatchGetImage",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetLifecyclePolicy",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ]
+      }
+    ]
+  }
+  EOF
+}
