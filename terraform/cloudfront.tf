cc51c59ed74794465efd1d7ed00d8628fd0fdb3b
Ben Orrin <skylonrow54@googlemail.com>
2022-10-17 20:00:43 +0100

    Added cloudfront 404 router

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -23,7 +23,13 @@ resource "aws_cloudfront_distribution" "solmines-cloudfront-dist" {
   }
 
   custom_error_response {
-    error_code = 403
+    error_code    = 403
+    response_code = 200
+    response_page_path = "/index.html"
+  }
+
+  custom_error_response {
+    error_code    = 404
     response_code = 200
     response_page_path = "/index.html"
   }
