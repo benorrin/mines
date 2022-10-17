53c6a898eebd338f0e29d165dcaebbdd8c72f372
Ben Orrin <skylonrow54@googlemail.com>
2022-10-17 11:55:14 +0100

    updated terraform provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,12 @@
 terraform {
-  required_version = ">=0.12.13"
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.34.0"
+    }
+  }
+
   backend "s3" {
     bucket         = "solmines-terraform-bucket"
     key            = "terraform.tfstate"
