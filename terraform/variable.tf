6717f52a6e105541ad7b189aed48eb971d8a11c8
Ben Orrin <skylonrow54@googlemail.com>
2022-10-17 10:43:38 +0100

    create terraform variables

---
+++ terraform/variable.tf
@@ -0,0 +1,13 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "db_username" {
+  default = "solmines-admin"
+  sensitive = true
+}
+
+variable "db_password" {
+  default = "JEfuSzv9XXRnGShWagXHwuAuXesENACPvrx87WAKbbCy4HkX8Zz3HMUeMirx2xkt"
+  sensitive = true
+}
