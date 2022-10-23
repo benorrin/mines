6cb0ba014076d1a5ba07fa96d3bdb942c9c1a318
Ben Orrin <skylonrow54@googlemail.com>
2022-10-23 22:17:40 +0100

    Revert "Added public subnet 2"
    This reverts commit d9b07045d7740ffaf284e84a2cc3e4a9e13fdd8b.

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -3,26 +3,14 @@
 ####
 
 # Create public subnet
-resource "aws_subnet" "solmines-public-subnet1" {
+resource "aws_subnet" "solmines-public-subnet" {
     vpc_id = "${aws_vpc.solmines-vpc.id}"
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true"
     availability_zone = "${var.region}a"
 
     tags = {
-        Name = "solmines-public-subnet1"
-    }
-}
-
-# Create public subnet2
-resource "aws_subnet" "solmines-public-subnet2" {
-    vpc_id = "${aws_vpc.solmines-vpc.id}"
-    cidr_block = "10.0.2.0/24"
-    map_public_ip_on_launch = "true"
-    availability_zone = "${var.region}b"
-
-    tags = {
-        Name = "solmines-public-subnet2"
+        Name = "solmines-public-subnet"
     }
 }
 
