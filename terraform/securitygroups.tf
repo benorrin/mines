b61fe8bf18e2004c1f0345133b7b2180977d8d20
Ben Orrin <skylonrow54@googlemail.com>
2022-10-23 22:17:46 +0100

    Revert "Add ELB security group"
    This reverts commit 0f5705d4d4a3a9612c53302869f57335f598d28d.

--- terraform/securitygroups.tf
+++ terraform/securitygroups.tf
@@ -20,22 +20,3 @@ vpc_id      = "${aws_vpc.solmines-vpc.id}"
     cidr_blocks = ["10.0.1.0/24"]
   }
 }
-
-# Create ELB security group 
-resource "aws_security_group" "solmines-elb" {
-vpc_id      = "${aws_vpc.solmines-vpc.id}"
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/24"]
-  }
-}
