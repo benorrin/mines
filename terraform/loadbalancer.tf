66049bd6327124f7e393ed2464d5c7949b696d2a
Ben Orrin <skylonrow54@googlemail.com>
2022-10-23 22:17:27 +0100

    Revert "fixed typo"
    This reverts commit 0da5932c736f29db3ec91f422f430c6ddc16b4c2.

--- terraform/loadbalancer.tf
+++ terraform/loadbalancer.tf
@@ -7,7 +7,7 @@ resource "aws_lb" "solmines-lb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.solmines-elb.id]
-  subnets            = ["${aws_subnet.solmines-public-subnet1.id}", "${aws_subnet.solmines-public-subnet2.id}"]
+  subnets            = ["${aws_subnet.solmines-public-subnet1.id}, ${aws_subnet.solmines-public-subnet2.id}"]
 
   enable_deletion_protection = true
 }
