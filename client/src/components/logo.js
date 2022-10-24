f5213aff97e207ace24cc43181f30fecaf3ba832
Ben Orrin <skylonrow54@googlemail.com>
2022-10-24 18:35:07 +0100

    update logo size

--- client/src/components/logo.js
+++ client/src/components/logo.js
@@ -4,7 +4,7 @@ import { Link, Box, Text } from "@chakra-ui/react";
 export default function Logo(props) {
     return (
         <Box {...props}>
-            <Text fontSize="4xl" fontWeight="bold">
+            <Text fontSize="3xl" fontWeight="bold">
                 <Link href='https://mines.orrin.uk' isExternal>
                     SolMines
                 </Link>
