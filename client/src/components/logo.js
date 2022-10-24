84c2019c580b9a3117a2f36aca72a2dd4af7269c
Ben Orrin <skylonrow54@googlemail.com>
2022-10-24 18:56:04 +0100

    update nav link

--- client/src/components/logo.js
+++ client/src/components/logo.js
@@ -4,7 +4,7 @@ import { Link, Box, Text } from "@chakra-ui/react";
 export default function Logo(props) {
     return (
         <Box {...props}>
-            <Text fontSize="3xl" fontWeight="bold">
+            <Text fontSize="2xl" fontWeight="bold">
                 <Link href='https://mines.orrin.uk'>
                     SolMines
                 </Link>
