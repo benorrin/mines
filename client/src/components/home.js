80c96387a79dd37abc2445524e03993809d19794
Ben Orrin <skylonrow54@googlemail.com>
2022-11-03 22:44:40 +0000

    Added home page template

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,6 +1,29 @@
+import {
+    Flex,
+    Stack,
+    Link,
+    Button,
+    Heading,
+    Text
+    } from '@chakra-ui/react';
+
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <Flex align={'center'} justify={'center'}>
+            <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+                <Stack align={'center'}>
+                    <Heading fontSize={'5xl'}>SolMines</Heading>
+                    <Text fontSize='3xl'>Classic Minesweeper With a Twist</Text>
+                </Stack>
+                <Stack spacing={8} align={'center'}>
+                <Link href='https://mines.orrin.uk/play'>
+                    <Button colorScheme='whatsapp' height='1em' width='100%'>
+                        Play Now
+                    </Button>
+                </Link>
+                </Stack>
+            </Stack>
+        </Flex>
 )};
 
 export default Home;
