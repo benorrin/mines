08fae83dd1f53aa6b6e70f76b6e3945bab5be57b
Ben Orrin <skylonrow54@googlemail.com>
2022-11-03 16:46:16 +0000

    Fixed backend logout route URL

--- api/routes/logout.js
+++ api/routes/logout.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const passport = require('passport');
 
-router.post('/logout', function(req, res, next) {
+router.post('/', function(req, res, next) {
     req.logout(function(err) {
         if (err) { return next(err); }
         res.json({
