ebc41cd4ea1350d55f444ecc34cfc0f4a7c8e36b
Ben Orrin <skylonrow54@googlemail.com>
2022-11-07 22:14:49 +0000

    add game model

---
+++ api/models/game.js
@@ -0,0 +1,85 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+mongoose.promise = Promise
+
+const GameSchema = new Schema({
+  game_id: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  user_id: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: Number,
+    required: true
+  },
+  bet: {
+    type: Number,
+    required: true
+  },
+  moves: {
+    type: Number,
+    required: true
+  },
+  gameboard: {
+    type: String,
+    required: true
+  },
+  revealed: {
+    type: String,
+    required: true
+  }
+});
+
+GameSchema.methods.isValidUser = async function(user_id) {
+  const game = this;
+  
+  if(this.user_id == user_id){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+GameSchema.methods.isValidGame = async function(gameID) {
+  const game = this;
+  
+  if(this.game_id == gameID){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+GameSchema.methods.isActive = async function() {
+  const game = this;
+  
+  if(this.status == 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+GameSchema.methods.getRevealed = async function() {
+  return JSON.parse(this.revealed);
+}
+
+GameSchema.methods.getGameboard = async function() {
+  return JSON.parse(this.gameboard);
+}
+
+GameSchema.methods.isRevealed = async function(square){
+  let revealed = JSON.parse(this.revealed);
+  if(revealed.hasOwnProperty(square)){
+    return true;
+  } else{
+    return false;
+  }
+}
+
+const Game = mongoose.model('Game', GameSchema);
+module.exports = Game;
