d1d496da5788ae5618c0bf9b042bcebc44a2fb45
Ben Orrin <skylonrow54@googlemail.com>
2022-11-07 21:21:26 +0000

    Add new game logic to newGame controller

--- api/controllers/newGame.js
+++ api/controllers/newGame.js
@@ -1,6 +1,8 @@
 const crypto = require('crypto');
 const GameModel = require('../models/game.js');
 const generateBoard = require('./generateBoard.js');
+const login = require('../controllers/login.js');
+
 
 /**
  * Creates a new game. Generates a random gameboard and creates new document in DB.
@@ -11,21 +13,36 @@ const generateBoard = require('./generateBoard.js');
  * @returns {string} game_id The unique game ID of the created game.
  */
 
-async function newGame(userID) {
-    let game_id = crypto.randomBytes(8).toString('hex');
-    let gameboard = await generateBoard(5, 5);
+async function newGame(userID, username, bet) {
 
-    const game = GameModel.create({
-        game_id: game_id,
-        user_id: userID,
-        status: 0,
-        bet: 0,
-        moves: 0,
-        gameboard: gameboard,
-        revealed: "{}"
-      });
+    const user = login.getUser(username);
 
-      return game_id;
+    const sufficientBalance = await user.checkBalance(bet);
+
+    if(!sufficientBalance) {
+        const error = new Error('Insufficient balance');
+        throw error;
+    }
+
+    try {
+        let game_id = crypto.randomBytes(8).toString('hex');
+        let gameboard = await generateBoard(5, 5);
+
+        const game = GameModel.create({
+            game_id: game_id,
+            user_id: userID,
+            status: 0,
+            bet: 0,
+            moves: 0,
+            gameboard: gameboard,
+            revealed: "{}"
+        });
+
+        return game_id;
+        
+    } catch(error){
+        throw error;
+    }
 }
 
 module.exports = newGame;
