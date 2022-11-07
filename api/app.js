69e898e406e3778e3f90a56e953315fe11a90da1
Ben Orrin <skylonrow54@googlemail.com>
2022-11-07 22:12:33 +0000

    Added further CORS declarations

--- api/app.js
+++ api/app.js
@@ -47,6 +47,11 @@ app.use(passport.session())
 *  App Declarations
 */
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(cors({
     allowedHeaders: ["authorization", "Content-Type"],
     exposedHeaders: ["authorization"],
