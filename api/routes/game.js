4962f595d018b48fe0d90a931b63b4653b9b6108
Ben Orrin <skylonrow54@googlemail.com>
2022-11-07 23:07:35 +0000

    Add move route to api

--- api/routes/game.js
+++ api/routes/game.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 const passport = require('passport');
 const newGame = require('../controllers/newGame.js');
+const moveGame = require('../controllers/moveGame.js');
+
 
 router.post('/new', async (req, res, next) => {
     try {
@@ -18,6 +20,22 @@ router.post('/new', async (req, res, next) => {
     } catch(error){
         next(error);
     }
+});
+
+
+router.post('/move', async (req, res, next) => {
+    let game_id = req.body.game_id;
+    let user = "";
+    let square = req.body.square;
+
+    let game = await moveGame(game_id, user, square);
+
+    res.json({
+        game_id: game_id,
+        moves: game[0],
+        revealed: game[1],
+        game_status: game[2]
+    })
 });
 
 module.exports = router;
