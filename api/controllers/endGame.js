18c938cbcc9e3f0204ca7f484720780c51db39e5
Ben Orrin <skylonrow54@googlemail.com>
2022-11-10 15:06:49 +0000

    Add API end game logic

---
+++ api/controllers/endGame.js
@@ -0,0 +1,39 @@
+const GameModel = require('../models/game.js');
+
+/**
+ * Ends a specific game submitted to the server
+ * 
+ * @param {string} gameID Unique identifier of the game the move will be processed on.
+ * @param {string} userID Unique identifier of the user submitting the move request.
+ * 
+ * @returns {array} gameBoard The generated gameboard in array form. 1 respresents mines, 0 represents free squares.
+ */
+
+async function moveGame(gameID, userID) {
+    const game = await GameModel.findOne({"game_id": gameID});
+    const user = await UserModel.findone({userID});
+
+    if(game.user_id != userID) {
+        const error = new Error('Invalid user');
+        throw error;
+    }
+
+    if(game.status === 1) {
+        const error = new Error('Game already ended');
+        throw error;
+    }
+
+    game.status = 1;
+    await game.save();
+
+    const winnings = game.bet * game.moves;
+
+    const newBalance = user.balance + winnings;
+
+    user.balance = newBalance;
+    await user.save();
+
+    return [ gameID, newBalance, game.status];
+}
+
+module.exports = moveGame;
